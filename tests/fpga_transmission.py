4f6be3e56579fa15bf5a8ced25d4c0d67f1258f3
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2023-11-28 11:25:13 +0300

    working on fpga transmission code

--- tests/fpga_transmission.py
+++ tests/fpga_transmission.py
@@ -4,8 +4,6 @@ amplitudes = []
 
 with open(r'tests\fpga_data.txt', 'r') as file:
     for line in file:
-        print('Line: ', line)
         line = line.strip()
-        print("Stripped line: ", line)
         if line.isnumeric():
             amplitudes.append(int(line))
