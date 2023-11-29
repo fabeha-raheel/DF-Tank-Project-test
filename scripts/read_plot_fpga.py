1c89101d8b317aee21eb30e806d660ae7698f898
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2023-11-29 10:56:08 +0300

    serially reading fpga data and plotting it

--- scripts/read_plot_fpga.py
+++ scripts/read_plot_fpga.py
@@ -4,55 +4,59 @@ import re
 import matplotlib.pyplot as plt 
 import numpy as np
 
-
 startSequence = r"^11111111 >> ([0-9]*)$"
 stopSequence = r"^([0-9]*) >> ([0-9]*)$"
-
 
 def request_antenna_data(device):
     
     amplitudes = []
 
-    command = '\n'
-    command = command.encode()
-    device.write(command)
-    
-    # now = time.time()
-    while True:
-        data = fpga.readline()
-        data = data.decode().strip()
-        # data = data.strip()
-        # print(data)
-        
-        startresult = re.search(startSequence, data)
-        stopresult = re.search(stopSequence, data)
+    if device.is_open:
 
-        if data.isnumeric():
-            amplitudes.append(int(data))
-        elif startresult is not None:
-            startfreq = int(startresult.groups()[0])
-        elif stopresult is not None:
-            endfreq = int(stopresult.groups()[1])
-            sampleSize = int(stopresult.groups()[0])
-        elif data == "*******************************************************************************":
+        command = '\n'
+        command = command.encode()
+        device.write(command)
+        
+        while True:
+            data = fpga.readline()
+            data = data.decode().strip()
+            if data=='\n':
+                continue
             
-            return startfreq, endfreq, sampleSize, amplitudes
-        # print("Time taken by if block: ", time.time()-now)
+            startresult = re.search(startSequence, data)
+            stopresult = re.search(stopSequence, data)
+
+            if data.isnumeric():
+                amplitudes.append(int(data))
+            elif startresult is not None:
+                startfreq = int(startresult.groups()[0])
+            elif stopresult is not None:
+                endfreq = int(stopresult.groups()[1])
+                sampleSize = int(stopresult.groups()[0])
+            elif data == "*******************************************************************************":
+                
+                return startfreq, endfreq, sampleSize, amplitudes
 
 # Connect to the FPGA over UART
-fpga = serial.Serial(
+print("Connecting to FPGA...")
+try:
+    fpga = serial.Serial(
     port='COM7',
     baudrate=115200,
     parity=serial.PARITY_NONE,
     timeout = None
     )
-time.sleep(3)
+    time.sleep(1)
+except serial.SerialException:
+    print("Error connecting to port.")
+finally:
+    print("Connection successfully established.")
+    print()
 
 print("Requesting data from FPGA...")
 now = time.time()
 startfreq, endfreq, sampleSize, amplitudes = request_antenna_data(fpga)
 print("Data received in {} seconds".format(time.time()-now))
-# print("Received Data")
 # print(amplitudes)
 # print("Start Freq: ", startfreq)
 # print("Stop Freq: ", endfreq)
@@ -60,9 +64,6 @@ print("Data received in {} seconds".format(time.time()-now))
 
 # plt.ion() 
 # fig = plt.figure()
-# now = time.time()
-x = list(np.arange(start=startfreq, stop=endfreq, step=((endfreq-startfreq)/sampleSize)))
-y = amplitudes
-plt.plot(x, y, '-b')
-# print("Data displayed in {} seconds.".format(time.time()-now))
+bandwidth = list(np.arange(start=startfreq, stop=endfreq, step=((endfreq-startfreq)/sampleSize)))
+plt.plot(bandwidth, amplitudes, '-c')
 plt.show()
